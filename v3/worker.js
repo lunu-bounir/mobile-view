d55550fbd31113d59ce82408ae4563ff4bc3eb4c
Lunu Bounir <lunu.bounir@gmail.com>
2025-07-05 23:03:43 -0700

    version 0.2.3

--- v3/worker.js
+++ v3/worker.js
@@ -46,60 +46,58 @@ chrome.contextMenus.onClicked.addListener(info => {
   }
 });
 
-chrome.action.onClicked.addListener(tab => {
-  chrome.storage.local.get({
+chrome.action.onClicked.addListener(async tab => {
+  const prefs = await chrome.storage.local.get({
     'mode': 'android',
-    'ua-android': 'Mozilla/5.0 (Linux; Android 13; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
-    'ua-ios': 'Mozilla/5.0 (iPhone14,3; U; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/19A346 Safari/602.1',
+    'ua-android': 'Mozilla/5.0 (Linux; Android 16) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.7204.46 Mobile Safari/537.36',
+    'ua-ios': 'MMozilla/5.0 (iPhone; CPU iPhone OS 17_7_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/138.0.7204.119 Mobile/15E148 Safari/604.1',
     'ua-kindle': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-us; Silk/1.0.146.3-Gen4_12000410) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16 Silk-Accelerated=true',
     'bypass-cache': false
-  }, async prefs => {
-    const rules = await chrome.declarativeNetRequest.getSessionRules();
-    if (rules.some(r => r.id === tab.id)) {
-      await chrome.declarativeNetRequest.updateSessionRules({
-        removeRuleIds: [tab.id]
-      });
-    }
-    else {
-      await chrome.declarativeNetRequest.updateSessionRules({
-        removeRuleIds: [tab.id],
-        addRules: [{
-          'id': tab.id,
-          'action': {
-            'type': 'modifyHeaders',
-            'requestHeaders': [{
-              'header': 'user-agent',
-              'operation': 'set',
-              'value': prefs['ua-' + prefs.mode]
-            }]
-          },
-          'condition': {
-            'tabIds': [tab.id],
-            'resourceTypes': [
-              'main_frame', 'sub_frame', 'stylesheet', 'script', 'image', 'font', 'object', 'xmlhttprequest', 'ping',
-              'csp_report', 'media', 'websocket', 'webtransport', 'webbundle', 'other'
-            ]
-          }
-        }]
-      });
-      chrome.action.setIcon({
-        tabId: tab.id,
-        path: {
-          '16': '/data/icons/active/16.png',
-          '32': '/data/icons/active/32.png',
-          '48': '/data/icons/active/48.png'
-        }
-      });
-    }
-    chrome.tabs.reload(tab.id, {
-      bypassCache: prefs['bypass-cache']
+  });
+  const rules = await chrome.declarativeNetRequest.getSessionRules();
+  if (rules.some(r => r.id === tab.id)) {
+    await chrome.declarativeNetRequest.updateSessionRules({
+      removeRuleIds: [tab.id]
     });
+  }
+  else {
+    await chrome.declarativeNetRequest.updateSessionRules({
+      removeRuleIds: [tab.id],
+      addRules: [{
+        'id': tab.id,
+        'action': {
+          'type': 'modifyHeaders',
+          'requestHeaders': [{
+            'header': 'user-agent',
+            'operation': 'set',
+            'value': prefs['ua-' + prefs.mode]
+          }]
+        },
+        'condition': {
+          'tabIds': [tab.id],
+          'resourceTypes': Object.values(chrome.declarativeNetRequest.ResourceType)
+        }
+      }]
+    });
+    chrome.action.setIcon({
+      tabId: tab.id,
+      path: {
+        '16': '/data/icons/active/16.png',
+        '32': '/data/icons/active/32.png',
+        '48': '/data/icons/active/48.png'
+      }
+    });
+  }
+  chrome.tabs.reload(tab.id, {
+    bypassCache: prefs['bypass-cache']
   });
 });
 
-chrome.declarativeNetRequest.setExtensionActionOptions({
-  displayActionCountAsBadgeText: true
-});
+if ('setExtensionActionOptions' in chrome.declarativeNetRequest) {
+  chrome.declarativeNetRequest.setExtensionActionOptions({
+    displayActionCountAsBadgeText: true
+  });
+}
 
 chrome.tabs.onRemoved.addListener(tabId => chrome.declarativeNetRequest.updateSessionRules({
   removeRuleIds: [tabId]
@@ -109,8 +107,7 @@ chrome.tabs.onRemoved.addListener(tabId => chrome.declarativeNetRequest.updateSe
 {
   const {management, runtime: {onInstalled, setUninstallURL, getManifest}, storage, tabs} = chrome;
   if (navigator.webdriver !== true) {
-    const page = getManifest().homepage_url;
-    const {name, version} = getManifest();
+    const {homepage_url: page, name, version} = getManifest();
     onInstalled.addListener(({reason, previousVersion}) => {
       management.getSelf(({installType}) => installType === 'normal' && storage.local.get({
         'faqs': true,
@@ -119,7 +116,7 @@ chrome.tabs.onRemoved.addListener(tabId => chrome.declarativeNetRequest.updateSe
         if (reason === 'install' || (prefs.faqs && reason === 'update')) {
           const doUpdate = (Date.now() - prefs['last-update']) / 1000 / 60 / 60 / 24 > 45;
           if (doUpdate && previousVersion !== version) {
-            tabs.query({active: true, currentWindow: true}, tbs => tabs.create({
+            tabs.query({active: true, lastFocusedWindow: true}, tbs => tabs.create({
               url: page + '?version=' + version + (previousVersion ? '&p=' + previousVersion : '') + '&type=' + reason,
               active: reason === 'install',
               ...(tbs && tbs.length && {index: tbs[0].index + 1})
